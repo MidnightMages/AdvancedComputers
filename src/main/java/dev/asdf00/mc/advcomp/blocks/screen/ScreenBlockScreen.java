00a810540c80a0a112ccd92fa10873e6c3b60420
GHXX <GHXX@gmx.at>
2024-04-29 22:05:38 +0200

    fix screen qui

--- src/main/java/dev/asdf00/mc/advcomp/blocks/screen/ScreenBlockScreen.java
+++ src/main/java/dev/asdf00/mc/advcomp/blocks/screen/ScreenBlockScreen.java
@@ -19,8 +19,8 @@ public class ScreenBlockScreen extends AbstractContainerScreen<ScreenMenu> {
     public ScreenBlockScreen(ScreenMenu pMenu, Inventory pPlayerInventory, Component pTitle) {
         super(pMenu, pPlayerInventory, pTitle);
         computerEntity = null;
-        this.imageWidth = 287;
-        this.imageHeight = 166;
+        this.imageWidth = 256;
+        this.imageHeight = 149;
     }
 
     @Override
