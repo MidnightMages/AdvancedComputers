d1a45df42040ea6ee3ce0613e4187472f2e47e7c
GHXX <GHXX@gmx.at>
2024-09-07 08:31:12 +0200

    support copying multiple bbmodels

--- copyAssets.ps1
+++ copyAssets.ps1
@@ -10,6 +10,8 @@ foreach ($suffix in $dstFolders){
 & "./createTintedCableTextures.ps1"
 
 Copy-Item -Path "./assetSources/textures/" -Destination "./src/main/resources/assets/advancedcomputers/textures/" -Recurse -Filter '*.png' -Force
-Copy-Item -Path "./assetSources/bbmodels/wan_router/*" -Destination "./src/main/resources/assets/advancedcomputers/textures/block/" -Recurse -Filter '*.png' -Force
 New-Item -ItemType Directory -Path "./src/main/resources/assets/advancedcomputers/models/block/" | Out-Null
-Copy-Item -Path "./assetSources/bbmodels/wan_router/*" -Destination "./src/main/resources/assets/advancedcomputers/models/block/" -Recurse -Filter '*.json' -Force
+foreach ($inFolder in (Get-ChildItem -Path "./assetSources/bbmodels/").Name){
+	Copy-Item -Path "./assetSources/bbmodels/$inFolder/*" -Destination "./src/main/resources/assets/advancedcomputers/textures/block/" -Recurse -Filter '*.png' -Force
+	Copy-Item -Path "./assetSources/bbmodels/$inFolder/*" -Destination "./src/main/resources/assets/advancedcomputers/models/block/" -Recurse -Filter '*.json' -Force
+}
