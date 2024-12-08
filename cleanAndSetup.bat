ac324d57fffe19d9986bea4674d1bdcfbacc6212
GHXX <GHXX@gmx.at>
2024-12-08 04:02:37 +0100

    fix/cleanup buildscripts

--- cleanAndSetup.bat
+++ cleanAndSetup.bat
@@ -1,11 +1,9 @@
 @echo off
 echo -------- Removing old files... -------- 
-cmd /c gradlew.bat clean
-echo -------- Setting up luajava for dev... --------
-rem cmd /c gradlew.bat setupDevDeps
+cmd /c gradlew.bat clean || exit /b
 echo -------- Setting up IDEA... --------
-cmd /c gradlew.bat genIntellijRuns
+cmd /c gradlew.bat genIntellijRuns || exit /b
 echo -------- Genning data... --------
-cmd /c gradlew.bat runData
+cmd /c gradlew.bat runData || exit /b
 echo Done!
 timeout /t 10
--- createReleaseBuild.bat
+++ createReleaseBuild.bat
@@ -1,15 +1,13 @@
 @echo off
 echo -------- Removing old files... -------- 
 cmd /c gradlew.bat clean || exit /b
+echo -------- Setting up IDEA... --------
+cmd /c gradlew.bat genIntellijRuns || exit /b
 echo -------- Updating assets... -------- 
 PowerShell -NoProfile -ExecutionPolicy Bypass -File "copyAssets.ps1" || exit /b
 echo -------- Genning data... --------
 cmd /c gradlew.bat runData || exit /b
 echo -------- Creating build... --------
 cmd /c gradlew.bat build || exit /b
-rem echo -------- Setting up dev deps again... --------
-rem cmd /c gradlew.bat setupDevDeps
-echo -------- Setting up IDEA... --------
-cmd /c gradlew.bat genIntellijRuns || exit /b
-echo Done!
+echo Done, output is in ./build/libs :)
 timeout /t 10
