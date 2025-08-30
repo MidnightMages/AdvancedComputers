5f0fb3e8c02949121dbfee92322191f3aab7bbe1
GHXX <GHXX@gmx.at>
2025-08-30 05:19:58 +0200

    cleanup

--- src/main/java/dev/asdf00/mc/advcomp/blocks/computer/ComputerTier.java
+++ src/main/java/dev/asdf00/mc/advcomp/blocks/computer/ComputerTier.java
@@ -1,24 +1,8 @@
 package dev.asdf00.mc.advcomp.blocks.computer;
 
-import dev.asdf00.mc.advcomp.AdvancedComputers;
-import dev.asdf00.mc.advcomp.RegistryBlockItemPair;
-import net.minecraft.util.StringRepresentable;
-import net.minecraft.world.level.block.Block;
-import org.jetbrains.annotations.NotNull;
-
-public enum ComputerTier implements StringRepresentable {
-    Wood(AdvancedComputers.COMPUTER_BLOCK_WOOD),
-    Iron(AdvancedComputers.COMPUTER_BLOCK),
-//    Gold,
-    Diamond(AdvancedComputers.COMPUTER_BLOCK_DIAMOND),
-    Netherite(AdvancedComputers.COMPUTER_BLOCK_NETHERITE);
-
-    ComputerTier(RegistryBlockItemPair<Block> blockType) {
-
-    }
-
-    @Override
-    public @NotNull String getSerializedName() {
-        return this.toString().toLowerCase();
-    }
+public enum ComputerTier {
+    Wood,
+    Iron,
+    Diamond,
+    Netherite
 }
