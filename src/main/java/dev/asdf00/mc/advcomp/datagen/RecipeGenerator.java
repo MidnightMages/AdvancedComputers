cbfcbd864a1eea4eccb1a22e470017a2bc61b988
GHXX <GHXX@gmx.at>
2025-12-19 00:44:27 +0100

    adjust device cable crafting recipe

--- src/main/java/dev/asdf00/mc/advcomp/datagen/RecipeGenerator.java
+++ src/main/java/dev/asdf00/mc/advcomp/datagen/RecipeGenerator.java
@@ -207,10 +207,11 @@ public class RecipeGenerator extends RecipeProvider {
                 .save(pWriter);
         shaped(AdvancedComputers.DEVICE_CABLE_BLOCK.blockItem().get(), 16)
                 .pattern("WWW")
-                .pattern("III")
+                .pattern("IRI")
                 .pattern("WWW")
                 .define('W', Items.BLUE_WOOL)
                 .define('I', Tags.Items.INGOTS_IRON)
+                .define('R', Tags.Items.DUSTS_REDSTONE)
                 .unlockedBy("item", has(Tags.Items.INGOTS_IRON))
                 .save(pWriter);
 
