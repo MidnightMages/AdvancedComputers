b1b188a4e2c5677e4fac9e3bf5e6be9807ae03d5
GHXX <GHXX@gmx.at>
2026-08-14 13:50:44 +0200

    add testing code for jukebox

--- src/main/java/dev/asdf00/mc/advcomp/blocks/adapter/redstone_io/AdapterBlock.java
+++ src/main/java/dev/asdf00/mc/advcomp/blocks/adapter/redstone_io/AdapterBlock.java
@@ -1,5 +1,6 @@
 package dev.asdf00.mc.advcomp.blocks.adapter.redstone_io;
 
+import dev.asdf00.mc.advcomp.AdvancedComputers;
 import dev.asdf00.mc.advcomp.blocks.BasePeripheralOrHostBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
@@ -8,8 +9,11 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
@@ -17,7 +21,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.Objects;
 
 public class AdapterBlock extends BasePeripheralOrHostBlock {
     public static final DirectionProperty FACING = DirectionProperty.create("facing");
@@ -49,5 +52,15 @@ public class AdapterBlock extends BasePeripheralOrHostBlock {
     public void appendHoverText(@NotNull ItemStack pStack, @Nullable BlockGetter pLevel, @NotNull List<Component> pTooltip, @NotNull TooltipFlag pFlag) {
         pTooltip.add(Component.translatable("tooltip.advancedcomputers.shared.notimplemented"));
         super.appendHoverText(pStack, pLevel, pTooltip, pFlag);
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level pLevel, @NotNull BlockState pState, @NotNull BlockEntityType<T> pBlockEntityType) {
+        if (pLevel.isClientSide())
+            return null;
+
+        return createTickerHelper(pBlockEntityType, AdvancedComputers.ADAPTER_BE.get(),
+                (pLevel1, pPos, pState1, pBlockEntity) -> pBlockEntity.tick(pLevel1, pPos, pState1));
     }
 }
