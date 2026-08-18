bdd35ead28f8037ebc35404573f3e5fa3eb0c34d
00asdf <christoph@00asdf.dev>
2026-08-19 01:38:30 +0200

    sync

--- src/main/java/dev/asdf00/mc/advcomp/lua/adapterapi/MethodDescription.java
+++ src/main/java/dev/asdf00/mc/advcomp/lua/adapterapi/MethodDescription.java
@@ -5,6 +5,5 @@ import dev.asdf00.jluavm.runtime.types.LuaObject;
 import java.lang.invoke.MethodHandle;
 import java.util.function.Function;
 
-record MethodDescription(MethodHandle handle, Function<LuaObject, Object>[] argTranslators,
-                         Function<Object, LuaObject> resultTranslator) {
+record MethodDescription(MethodHandle handle, Class<?>[] argTypes, Class<?> resultType) {
 }
--- src/main/java/dev/asdf00/mc/advcomp/utils/MiscUtil.java
+++ src/main/java/dev/asdf00/mc/advcomp/utils/MiscUtil.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
+import static dev.asdf00.jluavm.runtime.utils.UDTranslators.*;
+
 public class MiscUtil {
     // src: https://stackoverflow.com/questions/29076439/java-8-copy-directory-recursively/60621544#60621544
     public static void copyFolderRecursively(Path source, Path target, CopyOption... options) throws IOException {
@@ -36,7 +38,7 @@ public class MiscUtil {
         return rv.toString();
     }
 
-    public static Class<?> boxThatType(Class<?> type) {
+    public static Class<?> toBoxedType(Class<?> type) {
         if (Object.class.isAssignableFrom(type)) {
             return type;
         } else if (byte.class.equals(type)) {
@@ -66,6 +68,8 @@ public class MiscUtil {
             return LuaObject.nil();
         } else if (value instanceof LuaObject lo) {
             return lo;
+        } else if (value instanceof LuaObject[] larr) {
+            return LuaObject.of(larr);
         } else if (value instanceof Number num) {
             if (value instanceof Float || value instanceof Double) {
                 return LuaObject.of(num.doubleValue());
@@ -80,8 +84,70 @@ public class MiscUtil {
             return LuaObject.of(s);
         } else if (LuaUserData.class.isAssignableFrom(value.getClass())) {
             return LuaObject.of((LuaUserData) value);
+        } else if (Enum.class.isAssignableFrom(value.getClass())) {
+            return LuaObject.of(((Enum<?>) value).name());
         } else {
             throw new IllegalArgumentException("cannot convert to LuaObject: " + value);
         }
+    }
+
+    public static boolean isLuaObjectConvertible(Class<?> type) {
+        return type == LuaObject.class ||
+                type == LuaObject[].class ||
+                type == Boolean.class ||
+                type == Character.class ||
+                type == String.class ||
+                Number.class.isAssignableFrom(type) ||
+                LuaUserData.class.isAssignableFrom(type) ||
+                Enum.class.isAssignableFrom(type);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static Object convertToJavaType(Class<?> javaType, LuaObject value) {
+        if (javaType == LuaObject.class) {
+            return value;
+        } else if (javaType == Boolean.class) {
+            return lo2z(value);
+        } else if (javaType == Character.class) {
+            return lo2c(value);
+        } else if (javaType == String.class) {
+            return lo2st(value);
+        } else if (javaType == Byte.class) {
+            return lo2b(value);
+        } else if (javaType == Short.class) {
+            return lo2s(value);
+        } else if (javaType == Integer.class) {
+            return lo2i(value);
+        } else if (javaType == Long.class) {
+            return lo2l(value);
+        } else if (javaType == Float.class) {
+            return lo2f(value);
+        } else if (javaType == Double.class) {
+            return lo2d(value);
+        } else if (Enum.class.isAssignableFrom(javaType)) {
+            return lo2en((Class<? extends Enum>) javaType, value);
+        } else if (LuaUserData.class.isAssignableFrom(javaType)) {
+            return lo2ud((Class<? extends LuaUserData>) javaType, value);
+        } else {
+            throw shouldNotReach();
+        }
+    }
+
+    public static boolean isFromLuaObjectConvertible(Class<?> type) {
+        return type == LuaObject.class ||
+                type == Boolean.class ||
+                type == Character.class ||
+                type == String.class ||
+                Number.class.isAssignableFrom(type) ||
+                LuaUserData.class.isAssignableFrom(type) ||
+                Enum.class.isAssignableFrom(type);
+    }
+
+    public static Error shouldNotReach() {
+        throw new Error("should not reach here");
+    }
+
+    public static Error shouldNotReach(Object context) {
+        throw new Error("should not reach here with context: " + context);
     }
 }
