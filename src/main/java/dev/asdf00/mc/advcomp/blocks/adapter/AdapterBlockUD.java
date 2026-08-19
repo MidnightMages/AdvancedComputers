5770ff4e74f74380d143f59ed53f0216a0d0fe29
GHXX <GHXX@gmx.at>
2026-08-19 02:15:54 +0200

    add some more adapter api functions

--- src/main/java/dev/asdf00/mc/advcomp/blocks/adapter/AdapterBlockUD.java
+++ src/main/java/dev/asdf00/mc/advcomp/blocks/adapter/AdapterBlockUD.java
@@ -5,10 +5,14 @@ import dev.asdf00.jluavm.api.userdata.LuaDeserializer;
 import dev.asdf00.jluavm.exceptions.LuaJavaError;
 import dev.asdf00.jluavm.runtime.types.LuaObject;
 import dev.asdf00.jluavm.utils.ByteArrayReader;
+import dev.asdf00.mc.advcomp.blocks.computer.ComputerBlock;
+import dev.asdf00.mc.advcomp.blocks.computer.ComputerBlockEntity;
 import dev.asdf00.mc.advcomp.lua.components.BaseAcBlockEntityComponentUD;
 import dev.asdf00.mc.advcomp.lua.vm.LuaVirtualMachine;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.item.RecordItem;
+import net.minecraft.world.level.block.NoteBlock;
 import net.minecraft.world.level.block.entity.JukeboxBlockEntity;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.jetbrains.annotations.NotNull;
@@ -96,6 +100,110 @@ public class AdapterBlockUD extends BaseAcBlockEntityComponentUD<AdapterBlockEnt
         });
     }
 
+    @LuaCallable
+    public String getRecordName() {
+        return this.blockEntity.runOnTickThread(() -> {
+            var posToQuery = getTargetPosition();
+            var be = this.blockEntity.getLevel().getBlockEntity(posToQuery);
+            if (be instanceof JukeboxBlockEntity jbe) {
+                var firstItem = jbe.getFirstItem();
+                if (firstItem.isEmpty())
+                    return null;
+                if (firstItem.getItem() instanceof RecordItem re) {
+                    return re.getDisplayName().getString();
+                }
+                return jbe.getFirstItem().toString();
+            } else {
+                throw new LuaJavaError("not pointing at a jukebox");
+            }
+        });
+    }
+
+    @LuaCallable
+    public void restartPlaying() {
+        this.blockEntity.runOnTickThread(() -> {
+            var posToQuery = getTargetPosition();
+            var be = this.blockEntity.getLevel().getBlockEntity(posToQuery);
+            if (be instanceof JukeboxBlockEntity jbe) {
+                jbe.startPlaying();
+            } else {
+                throw new LuaJavaError("not pointing at a jukebox");
+            }
+            return null;
+        });
+    }
+
+
+    @LuaCallable
+    public String nb_getInstrument() {
+        return this.blockEntity.runOnTickThread(() -> {
+            var posToQuery = getTargetPosition();
+            var bs = this.blockEntity.getLevel().getBlockState(posToQuery);
+            if (bs.getBlock() instanceof NoteBlock) {
+                return bs.getValue(NoteBlock.INSTRUMENT).getSerializedName();
+            } else {
+                throw new LuaJavaError("not pointing at a noteblock");
+            }
+        });
+    }
+
+    @LuaCallable
+    public int nb_getNotePitch() {
+        return this.blockEntity.runOnTickThread(() -> {
+            var posToQuery = getTargetPosition();
+            var bs = this.blockEntity.getLevel().getBlockState(posToQuery);
+            if (bs.getBlock() instanceof NoteBlock) {
+                return bs.getValue(NoteBlock.NOTE);
+            } else {
+                throw new LuaJavaError("not pointing at a noteblock");
+            }
+        });
+    }
+
+    @LuaCallable
+    public void nb_setNotePitch(int x) {
+        this.blockEntity.runOnTickThread(() -> {
+            var posToQuery = getTargetPosition();
+            var bs = this.blockEntity.getLevel().getBlockState(posToQuery);
+            if (bs.getBlock() instanceof NoteBlock) {
+                if (!NoteBlock.NOTE.getPossibleValues().contains(x)) {
+                    throw new LuaJavaError("Pitch %s is not valid for this noteblock.".formatted(x));
+                }
+                blockEntity.getLevel().setBlock(posToQuery, bs.setValue(NoteBlock.NOTE, x), 3);
+            } else {
+                throw new LuaJavaError("not pointing at a noteblock");
+            }
+            return null;
+        });
+    }
+
+    @LuaCallable
+    public String c_getRunState() {
+        return this.blockEntity.runOnTickThread(() -> {
+            var posToQuery = getTargetPosition();
+            var bs = this.blockEntity.getLevel().getBlockState(posToQuery);
+            if (bs.getBlock() instanceof ComputerBlock) {
+                return bs.getValue(ComputerBlock.RUN_STATE).getSerializedName();
+            } else {
+                throw new LuaJavaError("not pointing at a computer");
+            }
+        });
+    }
+
+    @LuaCallable
+    public LuaObject c_getCrashReason() {
+        var rv = this.blockEntity.runOnTickThread(() -> {
+            var posToQuery = getTargetPosition();
+            var be = this.blockEntity.getLevel().getBlockEntity(posToQuery);
+            if (be instanceof ComputerBlockEntity cbe) {
+                var lvm = cbe.getLvm();
+                return lvm == null ? null : lvm.stopCode;
+            } else {
+                throw new LuaJavaError("not pointing at a computer");
+            }
+        });
+        return rv == null ? LuaObject.NIL : LuaObject.of(rv);
+    }
 
     @LuaDeserializer
     public static AdapterBlockUD luaDeserialize(LuaObject[] objs, ByteArrayReader reader, Queue<Runnable> postActions, Object additionalData) {
